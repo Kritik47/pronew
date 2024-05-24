12d397464a181e997bb4818d1197e2b61c9da964
KRITIK KUMAR <Kritiksahu958958@gmail.com>
2024-05-24 08:01:44 +0530

    UPDATE

--- src/MDMART/router/router.js
+++ src/MDMART/router/router.js
@@ -25,8 +25,8 @@ router.post('/api/stripe', async (req, res) => {
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'payment',
-            success_url: 'http://localhost:3000/success',
-            cancel_url: 'http://localhost:3000/cancel',
+            success_url: 'https://kritik-munda-12.netlify.app//success',
+            cancel_url: 'https://kritik-munda-12.netlify.app//cancel',
         });
         res.send({ id: session.id });
     } catch (error) {
